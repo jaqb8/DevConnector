50b44662f5ca2df2102ef920442e8b6b02a2fe74
jaqb8 <qba.anisz@gmail.com>
2019-10-17 20:55:00 +0200

    comment display & delete

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { addComment, removeComment } from '../../actions/post';
+import { addComment } from '../../actions/post';
 
 const CommentForm = ({ addComment, postId }) => {
   const [text, setText] = useState('');
